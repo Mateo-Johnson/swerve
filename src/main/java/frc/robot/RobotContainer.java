3b08a2ecdd66e9575c02e1ef7eea43ec035b3e85
Mateo Johnson <johnson.12@themetroschool.org>
2025-02-09 12:02:32 -0500

    added auto

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,23 +1,38 @@
 package frc.robot;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.subsystems.drivetrain.SwerveDriveSubsystem;
 
 public class RobotContainer {
+    private SendableChooser<Command> autoChooser;
     private final SwerveDriveSubsystem m_driveSubsystem = new SwerveDriveSubsystem();
-    private final CommandXboxController m_driverController = new CommandXboxController(0);
+    private final CommandXboxController primary = new CommandXboxController(0);
+    double xSpeed;
+    double ySpeed;
+    double rot;
 
     public RobotContainer() {
-        configureDefaultCommand(m_driveSubsystem, () -> {
-            m_driveSubsystem.drive(
-                -m_driverController.getLeftY(),
-                m_driverController.getLeftX(),
-                m_driverController.getRightX(),
-                true
-            );
-        });
+        autoChooser = AutoBuilder.buildAutoChooser();
+        SmartDashboard.putData("Auto Chooser", autoChooser);
+
+        //SLOW MODE
+        if (primary.b().getAsBoolean()) {
+            xSpeed = (primary.getLeftY() * 0.3);
+            ySpeed = (primary.getLeftX() * 0.3);
+            rot = (primary.getRightX() * 0.3);
+        } else {
+            xSpeed = (primary.getLeftY());
+            ySpeed = (primary.getLeftX());
+            rot = (primary.getRightX());
+        }
+
+        configureDefaultCommand(m_driveSubsystem, () -> {m_driveSubsystem.drive(-xSpeed, ySpeed, rot, true);});
     }
 
     private void configureDefaultCommand(Subsystem subsystem, Runnable defaultAction) {
@@ -29,6 +44,6 @@ public class RobotContainer {
     }
 
     public Command getAutonomousCommand() {
-        return null;
+        return autoChooser.getSelected();
     }
 }
